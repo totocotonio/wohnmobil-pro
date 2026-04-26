3875b0d118f1859c6365850c93ee6bc397e69985
totocotonio <info@michaely.de>
2026-04-26 18:08:00 +0200

    init: Wohnmobil Pro v1.0.0 – Nivelliertool, Checklisten, Kosten

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'womo-pro-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './fonts/rajdhani-400.woff2',
+  './fonts/rajdhani-500.woff2',
+  './fonts/rajdhani-600.woff2',
+  './fonts/rajdhani-700.woff2',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
